419838b5b3e105c913ca7f2888b2a9a90f504182
mutiarizkianti <mutiarizkianti04@gmail.com>
2025-08-26 23:51:32 +0700

    Update terbaru

--- resources/views/akademisi/index.blade.php
+++ resources/views/akademisi/index.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="container mx-auto p-8">
     <h1 class="text-3xl font-bold mb-4">Halaman Akademisi</h1>
-    <p class="text-gray-700">Selamat datang di halaman akademisi. Di sini akan ditampilkan konten khusus untuk akademisi.</p>
+    <p class="text-gray-700">Selamat datang di halaman akademisi. Di sini akan ditampilkan konten khusus untuk akademisi. Kalau mood</p>
 </div>
 @endsection
